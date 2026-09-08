578eae4b497863ac001209fa3719cf3d9a64e80e
deepinpoop <deepinpoop@proton.me>
2026-09-08 18:27:30 +0300

    Update main.go

--- unsplash/main.go
+++ unsplash/main.go
@@ -12,7 +12,7 @@ import (
 	"sync"
 )
 
-const unsplashAccessKey = "cjjFLji7SSrb6iQ01Et3Z9iHFq9CmSosMQkl1lK3Ha4"
+const unsplashAccessKey = "ajjFLji7SSrb6iQ01Et3Z9iHFq9CmSosMQkl1lK3Ha4"
 
 const (
 	cacheDir     = "img"
